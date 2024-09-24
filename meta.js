fe0160081c67c1d58789a155e8711912600cad37
Ricardo Lapeira <ralt46@gmail.com>
2024-09-24 19:06:02 +0700

    Add files via upload

---
+++ meta.js
@@ -0,0 +1,19 @@
+class MyMeta extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+                <title>Content Strategy Services</title>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+                <!-- empieza el font Jost-->
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+                <!--Termina el font-->
+                <link href="styles.css" rel="stylesheet">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        `
+    }
+}
+
+customElements.define('my-meta', MyMeta)
