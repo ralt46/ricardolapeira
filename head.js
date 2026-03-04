1d2e0256320422ccd2f50f2e0b8fdb953bb0d7cd
Ricardo Lapeira <ralt46@gmail.com>
2026-03-04 14:44:08 +0100

    Umami analytics update

--- head.js
+++ head.js
@@ -16,3 +16,10 @@ class MyHead extends HTMLElement {
 }
 
 customElements.define('my-head', MyHead)
+
+// Umami Analytics
+const umamiScript = document.createElement('script');
+umamiScript.async = true;
+umamiScript.src = 'https://website-kwm6ph0cf-ralt46s-projects.vercel.app/script.js';
+umamiScript.setAttribute('data-website-id', '97c9b19e-2c4e-460a-bb0a-f41c3a9643f4');
+document.head.appendChild(umamiScript);
